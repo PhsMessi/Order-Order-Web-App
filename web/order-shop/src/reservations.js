27ff321f9950d66a64831cbc554b696cb6a7e714
Edge Gonzaga <144799619+PhsMessi@users.noreply.github.com>
2026-01-07 23:30:11 +0800

    added new function for reservations

--- web/order-shop/src/reservations.js
+++ web/order-shop/src/reservations.js
@@ -18,3 +18,25 @@
 
 // Done testing displayReservations(), createReservation() API in Postman
 // Making API key and Securing Admin endpoints(Done)
+
+import { orderMenu } from "../public/data.js";
+
+displayProducts();
+
+function displayProducts() {
+  const productList = document.querySelector(".product-lists");
+  productList.innerHTML = "";
+
+  orderMenu.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.className = "item";
+
+    li.innerHTML = `
+        <img src="${item.url}" alt="${item.url}">
+        <p>${item.name}</p>
+        <button>Add to cart</button>
+    `;
+
+    productList.appendChild(li);
+  });
+}
